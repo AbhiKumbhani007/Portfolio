1c1a9ac4446b63e12bb970469521ea5058d1e341
Abhishek <abhishek@vruttitech.com>
2023-11-29 16:59:14 +0530

    chore: logo updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://svgshare.com/i/10GX.svg"
+        />
+      </Head>
+
       <body>
         <title>Abhishek Kumbhani</title>
         <Main />
