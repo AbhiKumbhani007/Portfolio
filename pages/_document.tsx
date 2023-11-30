58ce656b75898f1b6fe6a58dae55ebeece8e14e6
Abhishek <abhishek@vruttitech.com>
2023-11-30 17:03:33 +0530

    logo updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="https://iili.io/JxsTv0x.png"
-        />
+        <link rel="icon" type="image/x-icon" href="/logo.png" />
       </Head>
       <body>
         <title>Abhishek Kumbhani</title>
