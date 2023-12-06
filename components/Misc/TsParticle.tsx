f57150e1f14c672e1d966fd9f299e2caf28986da
Abhishek <abhishek@vruttitech.com>
2023-12-06 09:47:50 +0530

    particles reduced

--- components/Misc/TsParticle.tsx
+++ components/Misc/TsParticle.tsx
@@ -80,7 +80,7 @@ const BackgroundParticles = () => {
               enable: true,
               area: 800,
             },
-            value: 100,
+            value: 80,
           },
           opacity: {
             value: 0.2,
